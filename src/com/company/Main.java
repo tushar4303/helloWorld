d481cc1268709e3c9a4dad1ea88d11e16040169f
Tushar Padhy <tushar4303@pop-os.localdomain>
2021-09-09 15:02:44 +0530

    Sharing my first project!!

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello, World!");
+    }
+}
+
+
+//Make project
+//create class
+//Make main method
+//print something
